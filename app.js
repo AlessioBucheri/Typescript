7e4bebb4d0913a5c74ae385d7f6963c75a2557c5
AlessioBucheri <157613253+AlessioBucheri@users.noreply.github.com>
2024-05-23 19:17:10 +0200

    tsc app.ts

--- app.js
+++ app.js
@@ -37,23 +37,29 @@ var Piattaforma = /** @class */ (function () {
     return Piattaforma;
 }());
 // Istanziazione di oggetti ProfessionistaMedia
-var prof1 = new ProfessionistaMedia('Maria', 'Rossi', 'Giornalismo', 5, ['diritti umani', 'politica']);
-var prof2 = new ProfessionistaMedia('Anna', 'Bianchi', 'Regia', 8, ['documentari', 'cinema']);
-var prof3 = new ProfessionistaMedia('Laura', 'Verdi', 'Produzione', 3, ['musica', 'spettacolo']);
+var professionista1 = new ProfessionistaMedia('Serena', 'Cardinale', 'Giornalismo', 5, ['diritti umani', 'politica']);
+var professionista2 = new ProfessionistaMedia('Franca', 'Marini', 'Regia', 8, ['documentari', 'cinema']);
+var professionista3 = new ProfessionistaMedia('Laura', 'Costantini', 'Produzione', 3, ['musica', 'spettacolo']);
 // Istanziazione di oggetti ProgrammaFormazione
 var programma1 = new ProgrammaFormazione('Leadership Femminile', 'Programma per sviluppare competenze di leadership', 'Giornalismo', 30);
 var programma2 = new ProgrammaFormazione('Mentorship Tecnica', 'Mentorship per sviluppo competenze tecniche', 'Regia', 60);
+var programma3 = new ProgrammaFormazione('Empowering Women', 'Programma per raccontare la donna nel mondo moderno', 'Produzione', 40);
 // Le professioniste partecipano ai programmi di formazione
-prof1.partecipaProgramma(programma1);
-prof2.partecipaProgramma(programma1);
-prof3.partecipaProgramma(programma2);
+professionista1.partecipaProgramma(programma1);
+professionista2.partecipaProgramma(programma1);
+professionista3.partecipaProgramma(programma2);
 // Istanziazione di oggetti Piattaforma
 var piattaforma1 = new Piattaforma('Voce delle Donne', 'online', 'Piattaforma per promuovere la voce delle donne nei media', ['giornalismo', 'cinema', 'documentari']);
 var piattaforma2 = new Piattaforma('Media Innovazione', 'audiovisivo', 'Canale per contenuti innovativi', ['spettacolo', 'musica']);
+var piattaforma3 = new Piattaforma('Le dee nel mondo moderno', 'podcast', 'Canale per contenuti audiovisivi', ['spettacolo', 'musica']);
 // Pubblicazione dei contenuti da parte delle professioniste
-piattaforma1.pubblicaContenuto(prof1, 'Un articolo sui diritti umani');
-piattaforma1.pubblicaContenuto(prof2, 'Un documentario sulla politica');
-piattaforma2.pubblicaContenuto(prof3, 'Un servizio sulla musica emergente');
+piattaforma1.pubblicaContenuto(professionista1, 'Un articolo sui diritti umani');
+piattaforma1.pubblicaContenuto(professionista2, 'Un documentario sulla politica');
+piattaforma2.pubblicaContenuto(professionista3, 'Un servizio sulla musica emergente');
+piattaforma3.pubblicaContenuto(professionista1, "Atena: La Saggezza e il Potere della Leadership Femminile");
+piattaforma3.pubblicaContenuto(professionista2, "Artemide: L'Indipendenza e la Forza della Donna Selvaggia");
+piattaforma3.pubblicaContenuto(professionista3, "Afrodite: Amore, Bellezza e Autostima nel Mondo Contemporaneo");
 // Verifica del funzionamento della logica di collegamento
 console.log(programma1.elencoPartecipanti);
 console.log(programma2.elencoPartecipanti);
+console.log(programma3.elencoPartecipanti);
